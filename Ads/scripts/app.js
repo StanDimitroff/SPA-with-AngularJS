71ee3ed98cc94548307d0cdc18e164d4ec1a4335
StanDimitroff <standimitroff@gmail.com>
2014-12-30 14:59:02 +0200

    basic routes configured

--- Ads/scripts/app.js
+++ Ads/scripts/app.js
@@ -16,6 +16,32 @@ var app = angular
             {
                 templateUrl:'views/guest/registration.html'
             })
+//            -----USER-----
+            .when('/user/home',
+            {
+                templateUrl:'views/user/home.html'
+            })
+            .when('/user/ads',
+            {
+                templateUrl:'views/user/ads.html'
+            })
+            .when('/user/ads/publish',
+            {
+                templateUrl:'views/user/publish-ad.html'
+            })
+            .when('/user/ads/edit',
+            {
+                templateUrl:'views/user/edit-ad.html'
+            })
+            .when('/user/ads/delete',
+            {
+                templateUrl:'views/user/delete-ad.html'
+            })
+            .when('/user/profile',
+            {
+                templateUrl:'views/user/edit-profile.html'
+            })
+
             .otherwise({
                 redirectTo:'/home'
             })
