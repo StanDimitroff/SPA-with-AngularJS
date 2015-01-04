6168a3d93e8d02df8ea4286cf916b1ef1249260e
StanDimitroff <standimitroff@gmail.com>
2015-01-05 01:37:26 +0200

    forms validation added

--- Ads/scripts/controllers/login-controller.js
+++ Ads/scripts/controllers/login-controller.js
@@ -1,13 +1,18 @@
 app.controller('LoginController', function (rootUrl, $scope, authData, messages, $location) {
-    $scope.login = function (userData) {
-        authData.login(rootUrl, userData)
-            .then(function (data) {
-                $location.path('/user/home');
-            },
-            function () {
-                messages.errorMessage('Invalid login');
-            })
+    $scope.login = function (userData, loginForm) {
+        $scope.loginForm = loginForm;
+        $scope.user = userData;
 
+        if($scope.loginForm.$valid){
+            authData.login(rootUrl, userData)
+                .then(function (data) {
+                    $location.path('/user/home');
+                },
+                function () {
+                    messages.errorMessage('Invalid login');
+                    $scope.loginForm.$setPristine();
+                    $scope.user = null;
+                })
+        }
     }
-
 });
--- Ads/scripts/controllers/register-controller.js
+++ Ads/scripts/controllers/register-controller.js
@@ -3,16 +3,20 @@ app.controller('RegisterController', function (rootUrl, $scope, adsData, authDat
         .then(function (data) {
             $scope.towns = data;
         });
-    $scope.register = function (userData) {
-        authData.register(rootUrl, userData)
-            .then(function (data) {
-                messages.successMessage('User account created. Please login');
-                console.log(data);
-                $location.path('/login');
-            },
-            function () {
-                messages.errorMessage('Registration unsuccessful. Try again')
-            })
+    $scope.register = function (userData, registerForm) {
+        if (registerForm.$valid) {
+            authData.register(rootUrl, userData)
+                .then(function (data) {
+                    messages.successMessage('User account created. Please login');
+                    console.log(data);
+                    $location.path('/login');
+                },
+                function () {
+                    messages.errorMessage('Registration unsuccessful. Try again');
+                    $scope.loginForm.$setPristine();
+                    $scope.user = null;
+                })
+        }
 
     }
 });
