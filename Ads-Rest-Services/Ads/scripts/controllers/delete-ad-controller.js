4dfa9d72535f4f533efa2df0e9ab732e055e07bc
StanDimitroff <standimitroff@gmail.com>
2015-01-11 19:32:23 +0200

    commit fixes

---
+++ Ads-Rest-Services/Ads/scripts/controllers/delete-ad-controller.js
@@ -0,0 +1,29 @@
+﻿app.controller('DeleteAdController', function ($scope, Data, $routeParams, messages, $location, $http, $q) {
+    $scope.deleteAd = function (adData, adForm) {
+        adData.CategoryId = adData.categoryId;
+        adData.TownId = adData.townId;
+        Data.ads.delete($routeParams.id,$http, $q)
+        .then(function (data) {
+            messages.successMessage('Ad deleted successfully');
+            $location.path('/user/ads');
+        })
+    }
+    $scope.getAd = function () {
+        Data.common.getAllCategories($http, $q)
+         .then(function (data) {
+             $scope.categories = data;
+
+             Data.common.getAllTowns($http, $q)
+                .then(function (tdata) {
+                    $scope.towns = tdata;
+
+                    Data.ads.getById($routeParams.id, $http, $q)
+                        .then(function (adata) {
+                            $scope.ad = adata;
+                            console.log(data);
+                        });
+                })
+
+         })
+    }
+});
---
+++ Ads-Rest-Services/Ads/scripts/controllers/edit-profile-controller.js
@@ -0,0 +1,31 @@
+﻿app.controller('EditProfileController', function ($scope, Data, $routeParams, messages, $location, $http, $q) {
+    $scope.getUser = function () {
+        Data.user.getProfile($http, $q)
+            .then(function (data) {
+                console.log(data)
+                $scope.user = data;
+            })
+    }
+
+    Data.common.getAllTowns($http, $q)
+                .then(function (data) {
+                    $scope.towns = data;
+                })
+
+    $scope.editProfile = function (userData) {
+        Data.user.editProfile(userData, $http, $q)
+        .then(function (data) {
+            console.log(data);
+            messages.successMessage('Profile updated successfully');
+        })
+        console.log(userData)
+    }
+
+    $scope.changePassword = function (newPass) {
+        Data.user.changePassword(newPass, $http, $q)
+        .then(function (data) {
+            messages.successMessage('Password changed successfully')
+        })
+    }
+
+});
